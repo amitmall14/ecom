aad0c66122a8f14c73cdc45a6b8cbf9ff612f46f
amitmall14 <amitmalltech@gmail.com>
2022-11-21 22:41:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -14,12 +14,38 @@ function App() {
   return(
     <>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/Contactus" element={<Contactus />} />
-        </Routes>
-      </BrowserRouter>
+          <Routes>  
+          <Route  exact path= "/bootstrap" element= {<Bootstrap />} /> 
+          <Route  exact path= "/matarialui" element= {<Matarialui />} /> 
+         
+            
+            <Route  exact path= "/www" element= {<Routdd />} /> 
+             <Route  exact path= "/pnr" element= {<Pnr />} />   
+            <Route  exact path= "/login" element= {<Login name="amit" color="green"/>} /> 
+             <Route  exact path= "/signup" element= {<Login />} /> 
+            <Route  exact path= "/time" element= {<Timee />} />   
+            <Route  exact path= "/todo" element= {<Todo />} />  
+            <Route  exact path= "/fav" element= {<Fav />} />  
+            <Route  exact path= "/todo" element= {<Todo />} /> 
+            <Route  exact path= "/todoo" element= {<Todoo />} /> 
+            <Route  exact path= "/dressinfo" element= {<Dressinfo />} /> 
+            <Route  exact path= "/fib" element= {<Fib />} /> 
+            <Route  exact path= "/" element= {<HomePage />} /> 
+            {/* <Route path="*" element={<NoPage />} /> 
+
+            load Error page
+
+
+            */
+
+          }
+
+        {/*Redirect to the another Page*/}
+             <Route exact path ="*" element= {<Navigate to="/"replace={true} />} />
+
+             <Route  exact path= "/home" element= {<HomePage />} />      
+          </Routes>
+        </BrowserRouter>
    
     </>
     
